7409ebfb448dd0bb51047e812bef2db9d706d24f
Jamie Roberts <p.jamieroberts@yahoo.co.uk>
2021-01-05 13:56:22 +0000

    copy from anyline 27.1

---
+++ src/ios/ALNFCScanViewController.h
@@ -0,0 +1,26 @@
+//
+//  ALNFCScanViewController.h
+//  AnylineExamples
+//
+//  Created by Angela Brett on 08.10.19.
+//
+
+#import "ALPluginScanViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ALNFCScanViewController : UIViewController
+
+@property (nonatomic, assign) BOOL nativeBarcodeEnabled;
+
+@property (nonatomic, strong) NSString *cropAndTransformErrorMessage;
+
+@property (nonatomic, assign) NSUInteger quality;
+
+- (instancetype)initWithLicensekey:(NSString *)licensekey
+                     configuration:(NSDictionary *)anylineConfig
+              cordovaConfiguration:(ALCordovaUIConfiguration *)cordovaConf
+                          delegate:(id<ALPluginScanViewControllerDelegate>)delegate;
+@end
+
+NS_ASSUME_NONNULL_END
